2165f3dc776488e5bf8789b139e2510ab51bcb08
eduardperreca <eduardo.perreca@studenti.unimi.it>
2023-10-30 10:31:21 +0100

    first commit

--- personal_testing/Temperature/Temperature.java
+++ personal_testing/Temperature/Temperature.java
@@ -1,9 +1,36 @@
 public class Temperature {
 
     /*
-     * Attributes needed is just Kelvin
+     * Attributes kelvin, needed as value double.
      */
-    final double kevlin;
+    final double kelvin;
 
-    
+    private Temperature(double kelvin) {
+        if (kelvin > 0)
+            throw new IllegalArgumentException("Error: value cannot be negative");
+        this.kelvin = kelvin;
+    }
+
+    public static Temperature fromKelvin(double kelvin) {
+        return new Temperature(kelvin);
+    }
+
+    public static Temperature fromCelsius(double celsius) {
+        double kelvin = celsius + 275.15;
+        if (kelvin < 0)
+            throw new IllegalArgumentException("Error: value cannot be negative");
+        return new Temperature(kelvin);
+    }
+
+    public static Temperature fromFarenheit(double farenheit) {
+        double kelvin = (fahrenheit + 459.67) * 5.0 / 9.0;
+        if (kelvin < 0)
+            throw new IllegalArgumentException("Error: value cannot be negative");
+        return kelvin;
+    }
+
+    private boolean repok() {
+        return kelvin >= 0;
+    }
+
 }
